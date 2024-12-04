4cec631174b29593fa159b28b312de2370da5c23
Emanuell95 <131274256+Emanuell95@users.noreply.github.com>
2024-12-04 09:36:56 -0400

    Fixing the new main.h

--- _printf.c
+++ _printf.c
@@ -1,8 +1,39 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
- * _printf - print to stout formatted text
+ * _printf - Prints formatted text to stdout
+ * @format: Format specifier string
  *
- * @format: format specifier
- * Return: no of bytes printed
+ * Return: Number of characters printed
  */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int printed_chars = 0;
+
+    if (!format)
+        return (-1);
+
+    va_start(args, format);
+    while (*format)
+    {
+        if (*format == '%' && *(format + 1) == 'c') // Ejemplo básico: manejo de %c
+        {
+            format++;
+            char c = va_arg(args, int);
+            putchar(c);
+            printed_chars++;
+        }
+        else
+        {
+            putchar(*format);
+            printed_chars++;
+        }
+        format++;
+    }
+    va_end(args);
+
+    return (printed_chars);
+}
